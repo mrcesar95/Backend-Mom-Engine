853186a5ad6392a5025286e6e3afb74daa3118b7
Cesar Molina <cesaruxfxtx@gmail.com>
2022-06-29 09:50:41 -0500

    Adding .env

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const userController = require('../controllers/userController');
 
@@ -5,6 +6,12 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
+// router.get('/', (req, res) => {
+// 	response.send("<h1>Hello World!</h1>")
+// }),
+// router.get('/', (req, res) => {
+// 	response.send("<h1>Hello World!</h1>")
+// }),
 router.post('/singup', userController.singup);
 router.get('/confirm/:token', userController.confirmAccount);
 router.post('/login', userController.login)
